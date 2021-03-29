86bc4a7cabacfde7449ef785e345e01337010945
UnderAnder <andryushin.alexander@gmail.com>
2021-03-29 15:14:59 +0700

    Stage 2/4: GET vs POST

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, inputs
 import sys
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorldResource(Resource):
+@api.resource('/event/today')
+class TodayEvents(Resource):
     def get(self):
         return {"data": "There are no events for today!"}
 
 
-api.add_resource(HelloWorldResource, '/event/today')
+@api.resource('/event')
+class PostEvent(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'date',
+        type=inputs.date,
+        help='The event date with the correct format is required! The correct format is YYYY-MM-DD!',
+        required=True
+    )
+    parser.add_argument(
+        'event',
+        type=str,
+        help='The event name is required!',
+        required=True
+    )
+
+    def post(self):
+        args = self.parser.parse_args()
+        resp = {
+            "message": "The event has been added!",
+            "event": args['event'],
+            "date": str(args['date'].date())
+        }
+        return resp
+
 
 # do not change the way you run the program
 if __name__ == '__main__':
